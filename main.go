36d5a7827209cc75d8ce843d24b7b2c43297a146
test <cordial14@hanmail.net>
2022-11-26 12:00:30 +0900

    고 언어 공부 중

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "고 컨퍼런스"
@@ -14,33 +17,51 @@ func main() {
 	fmt.Printf("전체 티켓: %v 남은 티켓 %v\n", conferenceTickets, remainingTickets)
 	fmt.Println("당신의 표를 얻으세요")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	// ask user for their name
-	fmt.Println("당신의 이름을 입력하세요")
-	fmt.Scan(&firstName)
-	fmt.Println("당신의 성을을 입력하세요")
-	fmt.Scan(&lastName)
-	fmt.Println("당신의 이메일을 입력하세요")
-	fmt.Scan(&email)
-	fmt.Println("티켓을 입력하세요")
-	fmt.Scan(&userTickets)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		// ask user for their name
+		fmt.Println("당신의 이름을 입력하세요")
+		fmt.Scan(&firstName)
+		fmt.Println("당신의 성을을 입력하세요")
+		fmt.Scan(&lastName)
+		fmt.Println("당신의 이메일을 입력하세요")
+		fmt.Scan(&email)
+		fmt.Println("티켓을 입력하세요")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
+		if userTickets > remainingTickets {
+			fmt.Printf("We only hava %v tickets reamining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			break
+		}
 
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = remainingTickets - userTickets
 
-	fmt.Printf("The whole slice: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("The slice type: %T\n", bookings)
-	fmt.Printf("slice 길이: %v\n", len(bookings))
+		bookings = append(bookings, firstName+" "+lastName)
+		fmt.Printf("The whole slice: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("The slice type: %T\n", bookings)
+		fmt.Printf("slice 길이: %v\n", len(bookings))
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. you will 받을것이다 이메일 at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Thank you %v %v for booking %v tickets. you will 받을것이다 이메일 at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("These are all our bookings:%v\n", bookings)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names of bookings are:%v\n", firstNames)
+
+		if remainingTickets == 0 {
+			// end program
+			fmt.Println("Our conference is booked out. Come back next yaer.")
+			break
+		}
+	}
+
 }
 
 //1:12:31
